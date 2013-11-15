ad3da8f98d43419cdb7f38225a86280f0b2b5c9f
Yoana Todorova <todorova.yoana@gmail.com>
2013-11-15 20:49:00 +0200

    files added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Albavet::Application.config.secret_key_base = 'd27779430b0276abf2493f3cdb95f3da4fd1a550371e7738ee4031bc4cc97e72987d423dd1cd4864ebb0c2f6caa997b5124fe0d58a2dfda95772645d96d24664'
