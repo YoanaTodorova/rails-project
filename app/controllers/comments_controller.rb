a752b4553c6fb06c8c1c35baa14fb7dbea3587ad
YoanaTodorova <todorova.ioana@gmail.com>
2013-11-17 09:37:19 +0200

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,16 @@ class CommentsController < ApplicationController
    
     
     @player = Player.find(params[:player_id])
-    @comment = @player.comments.build params[:comment]
+    @comment = @player.comments.build comment_params
 
     @comment.author = "yoana"
     @comment.save
-    
+
     redirect_to :controller => "players", :action => "index"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author, :content)
   end
 end
