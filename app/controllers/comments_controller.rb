9ba771cbc78906297ab576b7f1c2a2d5403926df
YoanaTodorova <todorova.ioana@gmail.com>
2013-11-18 22:36:03 +0200

    add destroy method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,21 @@
 class CommentsController < ApplicationController
   
   def create
-   
-    
-    @player = Player.find(params[:player_id])
+    @player = Player.find params[:player_id]
     @comment = @player.comments.build comment_params
 
     @comment.author = "yoana"
     @comment.save
 
-    redirect_to :controller => "players", :action => "index"
+    redirect_to player_path @player
+  end
+
+  def destroy
+    @player = Player.find params[:player_id]
+    @comment = @player.comments.find params[:id]
+
+    @comment.destroy
+    redirect_to player_path @player
   end
 
   private
